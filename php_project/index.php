8fc587ea032b7d285af6932ca08c6107cdc63276
Win Khant Aung <leonzifer@gmail.com>
2023-05-20 08:49:50 +0630

    may 17

--- php_project/index.php
+++ php_project/index.php
@@ -17,11 +17,18 @@
 <body class="text-center">
     <div class="wrap">
         <h1 class="h3 mb-3">Login</h1>
+        <?php if (isset($_GET['success'])): ?>
+            <div class="alert alert-warning">
+                Register successful
+            </div>
+        <?php endif ?>
+
         <?php if (isset($_GET['incorrect'])): ?>
             <div class="alert alert-warning">
                 Incorrect Email or Password
             </div>
         <?php endif ?>
+
         <form action="_actions/login.php" method="post">
             <input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
             <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
---
+++ php_project/register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        .wrap {
+            width: 100%;
+            max-width: 400px;
+            margin: 40px auto;
+        }
+    </style>
+</head>
+
+<body class="text-center">
+    <div class="wrap">
+        <h1 class="h3 mb-3">Login</h1>
+        <?php if (isset($_GET['incorrect'])): ?>
+                    <div class="alert alert-warning">
+                        Something went wrong
+                    </div>
+        <?php endif ?>
+        <form action="_actions/create.php" method="post">
+            <input type="text" name="name" class="form-control mb-2" placeholder="Name" required>
+            <input type="text" name="email" class="form-control mb-2" placeholder="Email" required>
+            <input type="text" name="phone" class="form-control mb-2" placeholder="Phone" required>
+
+            <textarea type="text" name="address" class="form-control mb-2" placeholder="Address" required>
+                
+            </textarea>
+
+            <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
+            <button type="submit" class="w-100 btn btn-lg btn-primary">
+                Register
+            </button>
+        </form>
+        <br>
+        <a href="index.php">Login</a>
+    </div>
+</body>
+
+</html>
